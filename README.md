bb5520a64881cbf4cb57fea0581bd2cb41296357
singh <singh.omar97@gmail.com>
2018-01-12 14:54:01 +0100

    docs and readme

--- README.md
+++ README.md
@@ -9,6 +9,58 @@
 
 ![Docs](https://img.shields.io/badge/docs-2%25-blue.svg) ![Open](https://camo.githubusercontent.com/2091d99fb3b1ea0dcacb2ce564d5a3fc099c9ee7/68747470733a2f2f6261646765732e66726170736f66742e636f6d2f6f732f76322f6f70656e2d736f757263652e7376673f763d313032) ![License](https://camo.githubusercontent.com/056ff18a3d91ea14efa8b8c95ba9bc59d7c9676b/68747470733a2f2f6261646765732e66726170736f66742e636f6d2f6f732f67706c2f67706c2e7376673f763d313032)
 ![dependencies Status](https://david-dm.org/boennemann/badges/status.svg)
-### Re-Mixed enhanced version of the old flash Castle Battle game.
-#### Status
-Debugging and making docs
+### Story
+This game is made in C++ with Irrlicht engine for 3D graphic rendering and Bullet Engine for physic simulation. It is made for learning purpose and for an exam at university.
+### Prerequisite
+----------------
+* Castle battle is cross platform, but for now it only compile on Linux
+* [Irrlicht library](http://irrlicht.sourceforge.net)
+* [Bullet library](http://bulletphysics.org/wordpress)
+* C++11
+* This source
+
+### Compile release mode
+From the root directory of the project run
+```
+make
+```
+### Compile debug mode
+Don't forget to look Makefile for other command and for more specifications
+#### Debug level 1
+```
+make debug
+```
+#### Debug level 2
+```
+make debug2
+```
+#### Debug level n
+```
+make VERBOSITY=3 debug
+```
+
+### License
+```
+Copyright (c) 2018 Singh Amarjot
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+```
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,68 +6,10 @@
 #include <list>
 #include <Application.h>
 using namespace irr;
-
+/** Initialize seed and start ::Application */
 int main() {
   Application* app = new Application();
   srand (time(NULL));
   return (app->init()) ? app->loop() : 1;
 }
-
 #endif
-//
-//     IrrlichtDevice* device = createDevice(video::EDT_OPENGL,core::dimension2d<u32>(800,600),16, false,false,false,0);
-//     device->getCursorControl()->setVisible(false);
-//     video::IVideoDriver* driver = device->getVideoDriver();
-//     driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, true);
-//     scene::ISceneManager* smgr = device->getSceneManager();
-//     srand (time(NULL));
-//
-//     // core::stringw pathTexture[2] = {"media/terrain/grass_green_old.jpg","media/terrain/grass_green_thin.jpg"};
-//     // Terrain* terrain = new Terrain(smgr,driver,pathTexture,core::vector3df(250.f,0.0001f, 250.f));
-//     // Sky* sky = new Sky(smgr,driver,"media/terrain/sky.jpg",1000.0f);
-//
-//     /********************BULLET INIT***************************/
-//
-//     Physics* physics = new Physics(terrain->getTerrainBox());
-//
-//     /********************END BULLET INIT**********************/
-//
-//     //Player* player = new Player(device,smgr,driver,terrain->getCenter(), physics, Player::AI);
-//     GameManager* gameManager = new GameManager(smgr,driver,device,terrain->getCenter(), physics);
-//     //smgr->addCameraSceneNodeFPS(0,100.f,0.04f)->setPosition(terrain->getCenter());
-//     smgr->setAmbientLight(video::SColor(0,255,255,204));
-//     int lastFPS = -1;
-//     log(INFO,1,"CIAO");
-//     log(INFO,2,"CIAO hard");
-//
-//     u32 TimeStamp = device->getTimer()->getTime(), DeltaTime = 0;
-//     while(device->run() && device) {
-//
-// //        if(device->isWindowActive()){
-//             u32 fps = driver->getFPS();
-//             DeltaTime = device->getTimer()->getTime() - TimeStamp;
-//             physics->UpdatePhysics(DeltaTime,fps);
-//             driver->beginScene(true, true, video::SColor(255,200,200,200));
-//
-//             smgr->drawAll();
-//             if(gameManager->loop()){
-//                 gameManager->winner();
-//                 break;
-//             }
-//             driver->endScene();
-//
-//             TimeStamp = device->getTimer()->getTime();
-//
-//             if(lastFPS != fps)
-//             {
-//                 core::stringw str =L"Castle Battle [";
-//                 str += driver->getName();
-//                 str += "] FPS:";
-//                 str += fps;
-//                 device->setWindowCaption(str.c_str());
-//                 lastFPS = fps;
-//             }
-// //
-// //        } else
-// //            device->yield();
-//     }
